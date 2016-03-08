4f44dd3e085f6bba50d7b6eaefd4c237a7c4f795
adolenc <andrej.dolenc@student.uni-lj.si>
2016-03-08 18:25:34 +0100

    Added handling of other cases for simplification.

--- src/Logic.hs
+++ src/Logic.hs
@@ -6,9 +6,13 @@ import Data
 simplify :: LogicalFormula -> LogicalFormula
 simplify (Not (And ps)) = Or $ map (simplify . Not) ps
 simplify (Not (Or ps)) = And $ map (simplify . Not) ps
-simplify (Not (Not phi)) = phi
-simplify (Not phi) = Not (simplify phi)
-simplify phi = phi
+simplify (Not (Not p)) = p
+simplify (Not p) = Not (simplify p)
+simplify (And ps) = And $ map simplify ps
+simplify (Or ps) = Or $ map simplify ps
+simplify (Implies p q) = Implies (simplify p) (simplify q)
+simplify (Equiv p q) = Equiv (simplify p) (simplify q)
+simplify p = p
 
 eval :: LogicalFormula -> [(String, Bool)] -> Bool
 eval (And ps) values = True
