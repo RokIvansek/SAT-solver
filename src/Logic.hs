a8e023bd556e5663f43ec789fd786dec6bb46b22
adolenc <andrej.dolenc@student.uni-lj.si>
2016-03-08 18:22:23 +0100

    Implemented simplification of double negation and DeMorgan laws.

--- src/Logic.hs
+++ src/Logic.hs
@@ -4,6 +4,10 @@ import Data
 
 
 simplify :: LogicalFormula -> LogicalFormula
+simplify (Not (And ps)) = Or $ map (simplify . Not) ps
+simplify (Not (Or ps)) = And $ map (simplify . Not) ps
+simplify (Not (Not phi)) = phi
+simplify (Not phi) = Not (simplify phi)
 simplify phi = phi
 
 eval :: LogicalFormula -> [(String, Bool)] -> Bool
